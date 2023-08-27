0a876a1dce98bca5b66d1eb8223f758f84d20a4d
Maksim Semenov <maksimuma@list.ru>
2023-08-27 17:26:26 +0300

    feat: создать базовую структуру

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Main from '../Main/Main';
+// import Games from '../Games/Games';
+
+function App() {
+  return (
+    <div className="root">
+      <Main />
+      {/* <Games /> */}
+    </div>
+  );
+}
+
+export default App;
